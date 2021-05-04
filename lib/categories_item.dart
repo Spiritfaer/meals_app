fbf3935d63c8407d9cd8df24d30d6005c50d501b
Ihor Stalevskiy <ihor.stalevskiy@artmyweb.com>
2021-05-04 18:16:42 +0300

    add model and first screen

---
+++ lib/categories_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+class CategoriesItem extends StatelessWidget {
+  final Category category;
+
+  const CategoriesItem({@required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.headline1,
+      ),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.7),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15)),
+    );
+  }
+}
