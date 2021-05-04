323d68605e41c69589100b80a8702d59584a1995
Ihor Stalevskiy <ihor.stalevskiy@artmyweb.com>
2021-05-04 22:44:33 +0300

    end meal item

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/meal_item.dart';
 import '../models/category.dart';
 import '../dummy_data.dart';
 
@@ -21,7 +22,12 @@ class CategoryMealsScreen extends StatelessWidget {
         ),
         body: ListView.builder(
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                complexity: categoryMeals[index].complexity,
+                affordability: categoryMeals[index].affordability);
           },
           itemCount: categoryMeals.length,
         ));
