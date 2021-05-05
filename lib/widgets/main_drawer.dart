612b7f18298daf94184f0d97a6460647d4834b98
Ihor Stalevskiy <ihor.stalevskiy@artmyweb.com>
2021-05-06 00:12:13 +0300

    fixed main drawer with replacement route

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -50,14 +50,15 @@ class MainDrawer extends StatelessWidget {
             title: 'Meals',
             icon: Icons.restaurant,
             tapHandler: () {
-              Navigator.of(context).pushNamed('/');
+              Navigator.of(context).pushReplacementNamed('/');
             },
           ),
           _buildListTile(
             title: 'Filters',
             icon: Icons.settings,
             tapHandler: () {
-              Navigator.of(context).pushNamed(FiltersScreen.routeName);
+              Navigator.of(context)
+                  .pushReplacementNamed(FiltersScreen.routeName);
             },
           ),
         ],
